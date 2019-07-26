a0d8ac503f05cce3572e644a1da3bdefe1b9af64
Gauraviitkgp <gauravs123456789@gmail.com>
2019-07-26 07:08:27 +0100

    Added new header file

--- a.cpp
+++ a.cpp
@@ -4,41 +4,14 @@
 #include <math.h>
 #include <ctime>
 #include <unistd.h>
+#include <"rsa.h">
 #define ll long long
 using namespace std;
 
-#define PP 31
-#define QP 43
+#define CHANNEL_CAP 3000
+
 
 int count_nonzeros=0;
-
-void printA(int A[], int k, bool print_zeros)
-{
-    count_nonzeros=0;
-	for (int i = 0; i < k; i++)
-	{
-	    if(print_zeros)
-            cout<<"I:"<<i<<"\t\tA:"<<A[i]<<endl;
-        else
-        {
-            if(A[i]!=0)
-            {
-                cout<<"I:"<<i<<"\t\tA:"<<A[i]<<endl;
-                count_nonzeros++;
-            }
-        }
-
-	}
-}
-
-void countA(int A[], int k)
-{
-    count_nonzeros=0;
-	for (int i = 0; i < k; i++)
-        if(A[i]!=0)
-            count_nonzeros++;
-}
-
 
 class agent
 {
@@ -47,21 +20,6 @@ private:
 	int mhat;
     int *coprimes=NULL;
     int no_coprimes=0;
-	bool checkcoprime(int a, int b)
-	{
-	    if ( __gcd(a, b) == 1)
-	        return true;
-	    else
-	        return false;
-	}
-
-	int modInverse(int a, int m)
-	{
-	    a = a%m;
-	    for (int x=1; x<m; x++)
-	       if ((a*x) % m == 1)
-	          return x;
-	}
 
     void compute_coprimes()
     {
@@ -95,30 +53,6 @@ private:
 	}
 
 
-	int encrypt(int x, unsigned int y, int p)
-	{
-	    int res = 1;      // Initialize result
-
-	    x = x % p;  // Update x if it is more than or
-	                // equal to p
-
-	    while (y > 0)
-	    {
-	        // If y is odd, multiply x with result
-	        if (y & 1)
-	            res = (res*x) % p;
-
-	        // y must be even now
-	        y = y>>1; // y = y/2
-	        x = (x*x) % p;
-	    }
-	    return res;
-	}
-
-	int decrpyt(int Qhat)
-	{
-		return encrypt(Qhat,d,n);
-	}
 
 
 public:
@@ -185,6 +119,10 @@ void message_to_encryptionspace(int m, agent agent1, agent agent2, int A[], bool
 
 int main(int argc, char const *argv[])
 {
+	cout<<"Starting before actual Transmission codes"<<endl;
+	cout<<"Channel capacity is "<<CHANNEL_CAP<<endl;
+	print_list_of_avaiable_pq()
+
 	// struct timespec ts;
     // clock_gettime(CLOCK_MONOTONIC, &ts);
 
@@ -216,92 +154,6 @@ int main(int argc, char const *argv[])
     }
 
 
-    /*
-    int B[PP*QP+1]={0};
-    message_to_encryptionspace(3,agent1,agent2,B,false);
-    printA(B,1334,false);
-
-    int C[PP*QP+1]={0};
-    message_to_encryptionspace(9,agent1,agent2,C,false);
-    printA(C,1334,false);
-
-    int D[PP*QP+1]={0};
-    message_to_encryptionspace(10,agent1,agent2,D,false);
-    printA(D,1334,false);
-    */
-	/*
-	m=2;
-	cout<<"M:"<<m<<endl;
-	agent1.decrpyt_message_from(agent2);
-	agent1.get_decrypted_all_msg(m,A,false);
-    printA(A,1334,false);
-	cout<<"Non Zeros:"<<count_nonzeros<<endl<<endl;
-
-    m=3;
-    cout<<"M:"<<m<<endl;
-	A[1334]={0};
-	agent1.get_decrypted_all_msg(m,A,false);
-    printA(A,1334,false);
-	cout<<"Non Zeros:"<<count_nonzeros<<endl<<endl;
-
-    m=4;
-    cout<<"M:"<<m<<endl;
-	A[1334]={0};
-	agent1.get_decrypted_all_msg(m,A,false);
-    printA(A,1334,false);
-	cout<<"Non Zeros:"<<count_nonzeros<<endl<<endl;
-
-	m=37;
-    cout<<"M:"<<m<<endl;
-	A[1334]={0};
-	agent1.get_decrypted_all_msg(m,A,false);
-    printA(A,1334,false);
-	cout<<"Non Zeros:"<<count_nonzeros<<endl<<endl;
-	*/
-
-    /*
-	for(int i=30;i<44;i++)
-	{
-		m=6;
-		agent2.generate_encrypted_message(m,agent1);
-		agent2.Q=agent2.Q%i;
-		agent1.decrpyt_message_from(agent2);
-		cout<<"I:"<<i<<"\tM:"<<m<<"\tJ:"<<agent2.Q<<"\tMhat:"<<agent1.get_mhat()%i;
-		if (agent1.get_mhat()%i==m)
-			cout<<"\t\tMatch: True";
-		else
-			cout<<"\t\tMatch: False";
-		cout<<endl;
-
-		m=7;
-		agent2.generate_encrypted_message(m,agent1);
-		agent2.Q=agent2.Q%i;
-		agent1.decrpyt_message_from(agent2);
-		cout<<"I:"<<i<<"\tM:"<<m<<"\tJ:"<<agent2.Q<<"\tMhat:"<<agent1.get_mhat()%i;
-		if (agent1.get_mhat()%i==m)
-			cout<<"\t\tMatch: True";
-		else
-			cout<<"\t\tMatch: False";
-		cout<<endl;
-
-		m=35;
-		agent2.generate_encrypted_message(m,agent1);
-		agent2.Q=agent2.Q%i;
-		agent1.decrpyt_message_from(agent2);
-		cout<<"I:"<<i<<"\tM:"<<m<<"\tJ:"<<agent2.Q<<"\tMhat:"<<agent1.get_mhat()%i;
-		if (agent1.get_mhat()%i==m)
-			cout<<"\t\tMatch: True";
-		else
-			cout<<"\t\tMatch: False";
-		cout<<endl<<endl;
-		// usleep(10);
-	}
-	agent1.print_key(2971);
-	// print(A,1334);
-    */
-
-
-	// agent1.get_message_from(agent2);
 
 
 	return 0;
---
+++ rsa.h
@@ -0,0 +1,93 @@
+#include <iostream>
+#ifndef RSA_H // include guard
+#define RSA_H
+namespace 
+{
+	int primes[]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997}
+	
+	int count_nonzeros=0;
+	
+	void printarray(int A[],int size)
+	{
+		for (int i = 0; i < size; ++i)
+			cout<<"A["<<i<<"]:"<<A[i]<<endl;
+	}
+
+	void print_list_of_available_ops()
+	{
+		
+	}
+
+
+	void mappings(int map[], int size, bool print_zeros)
+	{
+		count_nonzeros=0;
+		if (print_zeros)
+			printarray(map,size);
+		else
+		{
+			for (int i = 0; i < size; i++)
+			{
+	            if(map[i]!=0)
+	            {
+	                cout<<"map["<<i<<"]:"<<map[i]<<endl;
+	                count_nonzeros++;
+	            }
+		     
+		    }
+
+		}
+	}
+
+	int count_non_zeros_array(int A[], int k)
+	{
+	    int count=0;
+		for (int i = 0; i < k; i++)
+	        if(A[i]!=0)
+	            count++;
+	    return count;
+	}
+
+	bool checkcoprime(int a, int b)
+	{
+	    if ( __gcd(a, b) == 1)
+	        return true;
+	    else
+	        return false;
+	}
+
+	int modInverse(int a, int m)
+	{
+	    a = a%m;
+	    for (int x=1; x<m; x++)
+	       if ((a*x) % m == 1)
+	          return x;
+	}
+
+	int encrypt(int x, unsigned int y, int p)
+	{
+	    int res = 1;      // Initialize result
+
+	    x = x % p;  // Update x if it is more than or
+	                // equal to p
+
+	    while (y > 0)
+	    {
+	        // If y is odd, multiply x with result
+	        if (y & 1)
+	            res = (res*x) % p;
+
+	        // y must be even now
+	        y = y>>1; // y = y/2
+	        x = (x*x) % p;
+	    }
+	    return res;
+	}
+	
+
+	int decrpyt(int Qhat)
+	{
+		return encrypt(Qhat,d,n);
+	}
+
+}
